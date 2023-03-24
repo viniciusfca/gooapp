27363f8e02c04877d9034d0e6a623286f1a40741
Vinicius <you@example.com>
2023-03-24 11:53:32 -0300

    Alterado mensagem

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/goapp", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello World Kong</h1>"))
+		w.Write([]byte("<h1>Hello World</h1>"))
 	})
 
 	http.ListenAndServe(":8080", nil)
